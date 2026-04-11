02a4188ff6b55be626750e84061736be7c296de1
Cookie <lucassrossi12@gmail.com>
2026-04-11 16:37:43 -0300

    changed supbase

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,4 +1,4 @@
 export const ENV = {
-    SUPABASE_ANON_KEY: import.meta.env.VITE_SUPABASE_ANON_KEY,
+    SUPABASE_ANON_KEY: import.meta.env.VITE_SUPBASE_ANON_KEY,
     SUPABASE_URL: import.meta.env.VITE_SUPABASE_URL,
 } as const
